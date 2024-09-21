10fe83079f5f8dce66244f5176c0d0147281a465
AveerG-7 <kingofcocaveer@gmail.com>
2024-09-21 09:45:28 +0530

    Set Intersection

---
+++ Sets and Arrays/Set Intersection.py
@@ -0,0 +1,9 @@
+#Write a program to find the intersection of two sets. Set1 = {green, blue} Set2 = {blue, yellow}
+
+Set1 = {"green","blue"}
+Set2 = {"blue", "yellow"}
+
+Interset = Set1.intersection(Set2)
+
+print(Interset)
+
