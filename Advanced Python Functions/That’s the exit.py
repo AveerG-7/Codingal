89d54ee792a51cc47f360692476d97e3ed2d85d0
AveerG-7 <kingofcocaveer@gmail.com>
2024-09-28 10:53:04 +0530

    List Comprehension

---
+++ Advanced Python Functions/That’s the exit.py
@@ -0,0 +1,16 @@
+#That’s the exit
+#Outline:
+#Write a program where the value of i begins from 1 and goes to 10. When the value of i becomes 5, force the interpreter to exit the program.
+
+for i in range(10):
+	
+	# If the value of i becomes
+	# 5 then the program is forced
+	# to exit
+	if i == 5:
+		
+		# prints the exit message
+		print(exit)
+		exit()
+	print(i)
+
