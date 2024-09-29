25c67eb046ccc35506f78158cf4365d630bc4ca7
AveerG-7 <kingofcocaveer@gmail.com>
2024-09-29 12:30:28 +0530

    Student class

---
+++ Object-Oriented Programming/Student class.py
@@ -0,0 +1,14 @@
+#Student Class
+#Outline:
+#Write a program to create a class with the name Student and perform the following tasks - 1. Declare a variable grade 2. Print a sentence inside the class 3. Create an object of class student and see the output
+
+class student:
+    grade = 7
+    print("a sentence")
+
+ob = student()
+
+
+
+
+
