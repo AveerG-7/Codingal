e3f5ec935aa10d7fabdbce669842f1b85100f6f4
AveerG-7 <kingofcocaveer@gmail.com>
2024-10-26 12:24:48 +0530

    challenges

--- Challenges in Object-Oriented Programming/Fruit Quiz.py
+++ Challenges in Object-Oriented Programming/Fruit Quiz.py
@@ -1,34 +1,28 @@
-import random
+import random 
 
 class FruitQuiz:
+    def __init__(self):
+        self.fruits = {'apple':'red','orange':'orange','mango':'yellow','banana':'yellow','watermelon':'green'}
 
-	# Create a constructor
-	def __init__(self):
-		
-		# Create a dictionary of fruits as keys and color as value
-		self.fruits={'apple':'red',
-					'orange':'orange',
-					'watermelon':'green',
-					'banana':'yellow'}
 
-	# Function for the quiz, here a fruit will be chosen randomly	
-	def quiz(self):
-		while (True):
-			
-			fruit, color = random.choice(list(self.fruits.items()))
-			
-			print("What is the color of {}".format(fruit))
-			user_answer = input()
-			
-			if(user_answer.lower() == color):
-				print("Correct answer")
-			else:
-				print("Wrong answer")
-				
-			option = int(input("enter 0 , if you want to play again otherwise enter 1: "))
-			if (option):
-				break
+    def quiz(self):
+        while(True):
+            fruit,color = random.choice(list(self.fruit.items()))
 
-print("welcome to fruit quiz ")
+            print("What is the colour of {}".format(fruit))
+            ans = input()
+            if (ans.lower() == color ):
+                print("answer is correct")
+            else:
+                print("answer is wrong")
+
+            option = int(input("Enter 0 if you want to play more else enter 1"))
+            if (option):
+                break
+
+print("Welcome to the fruit quiz")
 fq = FruitQuiz()
 fq.quiz()
+
+
+
