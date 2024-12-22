c5de41e438b553595ec10fafe11ff5efd25f3b22
AveerG-7 <kingofcocaveer@gmail.com>
2024-12-22 12:30:56 +0530

    text editor

--- Let’s Build a Text Editor/My text editor.py
+++ Let’s Build a Text Editor/My text editor.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 window = Tk()
-window.title("Codingal's Text Editor")
+window.title("Text Editor")
 window.geometry("600x500")
 window.rowconfigure(0, minsize=800, weight=1)
 window.columnconfigure(1, minsize=800, weight=1)
